a99b131b96092f047fa095fc6e121773683398ba
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 18:05:58 +0700

    update user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 const { Model } = require('sequelize');
-
+const { hashPassword } = require('../helpers/bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -20,7 +20,12 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
-
+    hooks: {
+      beforeCreate: (user, opt) => {
+        const hashedPassword = hashPassword(user.password)
+        user.password = hashedPassword
+      }
+    }
   });
   return User;
 };
