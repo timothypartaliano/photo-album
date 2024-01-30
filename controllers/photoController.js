367824e1ef945502a79317061c5b242333933018
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 14:15:51 +0700

    add GetByid Controller

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -10,6 +10,17 @@ class PhotoController {
                 res.status(500).json(err)
             })
     }
+
+    static GetOnePhotoByID(req, res) {
+        let id = +req.params.id
+        Photo.findByPk(id)
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json(err)
+            })
+    }
 }
 
 module.exports = PhotoController
