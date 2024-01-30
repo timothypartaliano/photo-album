55972421f5fb549462210d2d2a865b35f268790d
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 15:02:13 +0700

    add user migration

---
+++ migrations/20240130075231-create_foreign_key_userid_for_photo_table.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Photos", "userId", {
+      type: Sequelize.INTEGER
+    })
+
+    await queryInterface.addConstraint("Photos", {
+      fields: ["userId"],
+      type: "foreign key",
+      name: "user_fk",
+      references: {
+        table: "Users",
+        field: "id"
+      },
+      onDelete: "cascade",
+      onUpdate: "cascade"
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint("Photos", "user_fk")
+    await queryInterface.removeColumn("Photos", "userId")
+  }
+};
