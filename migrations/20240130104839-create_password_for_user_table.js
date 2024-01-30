bb813103b3ac6b260b4b32120530164c491a32cc
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 17:51:11 +0700

    add password field

---
+++ migrations/20240130104839-create_password_for_user_table.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'password', {
+      type: Sequelize.STRING
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
