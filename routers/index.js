51bae12b7d4b1111ce43b40edf906001ea236405
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 14:12:33 +0700

    add photoById route

--- routers/index.js
+++ routers/index.js
@@ -4,4 +4,6 @@ const PhotoController = require('../controllers/photoController')
 
 router.get('/photos', PhotoController.GetAllPhotos)
 
+router.get('/photos/:id', PhotoController.GetOnePhotoByID)
+
 module.exports = router
