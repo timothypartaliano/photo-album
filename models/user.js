d16fce0669f530156e7332a15f788f458726a462
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-30 18:05:19 +0700

    add bcrypt func

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+
   });
   return User;
 };
