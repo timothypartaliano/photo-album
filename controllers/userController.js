ef8beed4f83fd5e10120bafcbc4a18afc7f87a3f
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-31 10:23:43 +0700

    add login route & func

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { comparePassword } = require('../helpers/bcrypt')
 const { User } = require('../models')
 
 class UserController {
@@ -19,6 +20,39 @@ class UserController {
             .catch(err => {
                 res.status(500).json(err)
                 console.log(err);
+            })
+    }
+
+    static Login(req, res) {
+        const { email, password } = req.body
+        User.findOne({
+            where: {
+                email
+            }
+        })
+            .then(user => {
+                if (!user) {
+                    throw {
+                        name: "User Login Error",
+                        devMessage: `User with email ${email} not found`
+                    }
+                }
+                const isCorrect = comparePassword(password, user.password)
+                if (!isCorrect) {
+                    throw {
+                        name: "User Login Error",
+                        devMessage: `User's password with email ${email} does not match`
+                    }
+                }
+                let response = {
+                    id: user.id,
+                    username: user.username,
+                    email: user.email
+                }
+                return res.status(200).json(response)
+            })
+            .catch(err => {
+                res.status(401).json(err)
             })
     }
 }
--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,8 @@ const PhotoController = require('../controllers/photoController')
 const UserController = require('../controllers/userController')
 
 router.post('/users/register', UserController.register)
+router.post('/users/login', UserController.Login)
+
 router.get('/photos', PhotoController.GetAllPhotos)
 router.get('/photos/:id', PhotoController.GetOnePhotoByID)
 router.post('/photos', PhotoController.CreatePhoto)
