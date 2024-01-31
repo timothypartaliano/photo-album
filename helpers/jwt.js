809450197b2fb0114cb3f036fafaf6087f58b563
Timothy Partaliano <timothypartaliano@gmail.com>
2024-01-31 11:51:45 +0700

    add verifyToken func

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,17 @@
 const jwt = require("jsonwebtoken")
 const SECRET_KEY = "inirahasia"
 
-function generateToken(payload){
+function generateToken(payload) {
     const token = jwt.sign(payload, SECRET_KEY)
     return token
 }
 
+function verifyToken(token) {
+    const decoded = jwt.verify(token, SECRET_KEY)
+    return decoded
+}
+
 module.exports = {
-    generateToken
+    generateToken,
+    verifyToken
 }
